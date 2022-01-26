644cb21cdfbe7fbc11022edf3b91378fd285b09e
Ondřej Zobal <ondrej@ondrejzobal.xyz>
2022-01-26 18:45:30 +0100

    Added conifg and admin conosole

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
+#!/usr/bin/env node
+
 /// Buildins
 const path = require('path')
 const fs = require('fs')
 
 /// Installed
 const mysql = require('mysql')
-/// Express
 const express = require('express')
-const app = express()
 
+/// Configuration
+let config_path = './config.json'
 /// Constants
-const SQL_LOGIN_PATH = 'sql.json.credentials'
+let sql_login_path = './sql_login.json.credentials'
 
 /// FIXME debug constants
 var articles = [
   {
    title: "Hello wordl",
    author: "Ondřej Zobal",
-   content: "What is going on gang!",
+   content: "Sugonthose nuts",
   },
 
   {
@@ -40,17 +42,33 @@ var articles = [
 
 ]
 
-/// SQL Connection
-let sql_login = fs.readFileSync(SQL_LOGIN_PATH)
+
+/// Loading Configuration
+let config = fs.readFileSync(config_path)
+config = JSON.parse(config)
+
+/// Connecting to the SQL database
+let sql_login = fs.readFileSync(sql_login_path)
 sql_login = JSON.parse(sql_login)
 
+// Test query
 const sql_con =  mysql.createConnection(sql_login)
 sql_con.connect((err) => {
   if (err) throw err
-  else console.log("connected")
+  else {
+    console.log("connected")
+    sql_con.query("SELECT * FROM Author WHERE nameAuthor LIKE \"Ondřej\"", (err, result, fields) => {
+      if (err) throw err
+      console.log(result)
+      //console.log(fields)
+    })
+  }
 })
 
-/// Web server
+
+/// Public web
+const app = express()
+// Starting the webserver
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
@@ -70,4 +88,15 @@ app.all('*', (req, res) => {
 
 app.listen(5000, ()=> {
   console.log('Beeing a dirty baka!')
+})
+
+/// Admin web
+const admin = express()
+admin.set('view engine', 'ejs')
+admin.all('*', (req, res) => {
+  res.status(200)
+  res.sendFile(path.resolve(__dirname, './404.html'))
+})
+admin.listen(5001, () => {
+  console.log('Starting admin panel')
 })
